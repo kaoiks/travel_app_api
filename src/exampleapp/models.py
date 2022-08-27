f2a5ced05c27f4ebf8a2d265260ad0319bb36700
jakub-mrow <jakub@mrowczynski.net>
2022-08-28 00:56:25 +0200

    added example models with viewsets

--- src/exampleapp/models.py
+++ src/exampleapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=64, unique=True, help_text="user full name")
+    role = models.CharField(max_length=32, blank=True, null=True, help_text="user role")
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    description = models.CharField(max_length=128, help_text="Task description")
--- src/exampleapp/views.py
+++ src/exampleapp/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from exampleapp import models, serializers
+
+from rest_framework import permissions, status, viewsets
+from rest_framework.response import Response
+
+
+logger = logging.getLogger(__name__)
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    Return information about task.
+    """
+
+    serializer_class = serializers.TaskSerializer
+    queryset = models.Task.objects.all()
+
+    def create(self, request):
+        """
+        Submit a new task.
+        """
+        task_serializer = serializers.TaskSerializer(data=request.data)
+        task_serializer.is_valid(raise_exception=True)
+
+        logging.info("Creating task")
+        # task submit
+        task = models.Task()
+        task.description = request.data.get("description")
+        task.save()
+        logging.info("Task created")
+
+        return Response({"msg": "Task created"})
+
+
+    def list(self, request):
+        """
+        List all the tasks.
+        """
+        qs = models.Task.objects.all()
+        task_serializer = serializers.TaskSerializer(qs, many=True)
+
+        return Response(task_serializer.data)
