d280e962a98441f5b23c1778f023ca8f245400a1
jakub-mrow <jakub@mrowczynski.net>
2022-09-04 19:01:57 +0200

    modified pytest fixtures to support api authentication

--- src/conftest.py
+++ src/conftest.py
@@ -1,7 +1,22 @@
 import pytest
 
+from django.contrib.auth.models import User
+
 from rest_framework.test import APIClient
+
 
 @pytest.fixture
 def client():
-    return APIClient()
+    client = APIClient()
+
+    username = 'pytest-superuser'
+    password = 'pytest-superuser'
+    pytest_user = User.objects.create_superuser(username=username, password=password)
+    data = {'username': username, 'password': password, 'email': pytest_user.email}
+
+    response = client.post('/auth/login/', data=data)
+    token = response.json().get('access')
+
+    client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+    return client
