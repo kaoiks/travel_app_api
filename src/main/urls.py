7e720faa26a00b5d1696fcc453860033ec0cde9c
jakub-mrow <jakub@mrowczynski.net>
2022-09-27 22:19:40 +0200

    comments to urls

--- src/main/urls.py
+++ src/main/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include
 from django.urls import re_path
 
+
+# api and auth routes
 urlpatterns = [
     re_path("^api/", include("exampleapp.urls")),
     re_path("^auth/", include("authconf.auth_urls"))
